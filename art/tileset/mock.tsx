b0c322046dd5e96fc2dc5be6c44f8442aa00225d
Erick Zanardo <erickzanardoo@gmail.com>
2019-11-21 20:12:56 -0300

    Adding tileset

---
+++ art/tileset/mock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="../tileset.png" width="160" height="160"/>
+</tileset>
