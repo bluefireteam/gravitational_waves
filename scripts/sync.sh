2294174dbaf06ed7cc72539ea7f79c0c5f09a650
Luan Nico <luanpotter27@gmail.com>
2019-12-03 23:06:35 -0500

    adding player sprite

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,3 +2,7 @@
 
 cp ./art/tileset/tileset.png ./game/assets/images/
 cp ./art/tileset/tileset.json ./game/assets/images/
+
+cp ./art/char/char.png ./game/assets/images/
+cp ./art/char/char.json ./game/assets/images/
+
