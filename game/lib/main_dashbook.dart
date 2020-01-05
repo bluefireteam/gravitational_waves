a43c405da5ba161d1f5d2fcf7e7584c3ac20a4e2
Erick Zanardo <erickzanardoo@gmail.com>
2020-01-05 15:58:58 -0300

    Adding SpritesheetContainer

--- game/lib/main_dashbook.dart
+++ game/lib/main_dashbook.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:dashbook/dashbook.dart';
 
-import './widgets/button.dart';
+import 'package:flame/flame.dart';
 
-void main() {
+import './widgets/button.dart';
+import './widgets/label.dart';
+import './widgets/spritesheet_container.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final dashbook = Dashbook();
 
   dashbook
       .storiesOf('Button')
       .decorator(CenterDecorator())
-      .add('default', Button(label: 'Play', onPress: () {}))
-      .add('primary', PrimaryButton(label: 'Play', onPress: () {}))
-      .add('secondary', SecondaryButton(label: 'Play', onPress: () {}));
+      .add('default', (_) => Button(label: 'Play', onPress: () {}))
+      .add('primary', (_) => PrimaryButton(label: 'Play', onPress: () {}))
+      .add('secondary', (_) => SecondaryButton(label: 'Play', onPress: () {}));
+
+  final spriteSheet = await Flame.images.load('container-tileset.png');
+
+  dashbook
+      .storiesOf('SpritesheetContainer')
+      .decorator(CenterDecorator())
+      .add('default', (ctx) => Container(
+              width: ctx.numberProperty("width", 100),
+              height: ctx.numberProperty("height", 100),
+              child: SpritesheetContainer(
+                  spriteSheet: spriteSheet,
+                  tileSize: 16,
+                  destTileSize: 50,
+
+                  child: Center(child: Label(label: "Cool label")),
+              ))
+      );
 
   runApp(dashbook);
 }
