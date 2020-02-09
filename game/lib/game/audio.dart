d870106785b282584d648cad9e6bebd5e7bbd111
Erick Zanardo <erickzanardoo@gmail.com>
2020-02-09 14:51:31 -0300

    Fixing on iOS

--- game/lib/game/audio.dart
+++ game/lib/game/audio.dart
@@ -1,19 +1,11 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flame/audio_pool.dart';
 import 'package:flame/flame.dart';
 import 'package:gravitational_waves/game/preferences.dart';
 
 import 'util.dart';
 
 class Audio {
-  static final Map<String, AudioPool> pools = {
-    'coin.wav':
-        AudioPool('coin.wav', minPlayers: 2, maxPlayers: 2, repeating: false),
-    'die.wav':
-        AudioPool('die.wav', minPlayers: 1, maxPlayers: 1, repeating: false),
-  };
-
   static final AudioCache musicPlayer = _createLoopingPlayer(prefix: 'audio/');
 
   static AudioCache _createLoopingPlayer({String prefix}) {
@@ -25,7 +17,6 @@ class Audio {
   static Future init() async {
     Flame.audio.disableLog();
     if (!ENABLE_AUDIO) return;
-    await Future.wait(pools.values.map((p) => p.init()));
   }
 
   static void coin() {
@@ -36,15 +27,11 @@ class Audio {
     sfx('die.wav');
   }
 
-  static Stoppable sfx(String sound, {double volume = 1.0}) {
-    if (!ENABLE_AUDIO) return () {};
-    if (!Preferences.instance.soundOn) return () {};
-    final close = _startAsync(sound, volume: volume);
-    return () async => (await close)();
-  }
+  static void sfx(String sound, {double volume = 1.0}) {
+    if (!ENABLE_AUDIO) return;
+    if (!Preferences.instance.soundOn) return;
 
-  static Future<Stoppable> _startAsync(String sound, {double volume}) {
-    return pools[sound].start(volume: volume);
+    Flame.audio.play('sfx/$sound');
   }
 
   static void music(String song) async {
