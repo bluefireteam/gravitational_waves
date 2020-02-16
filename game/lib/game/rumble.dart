122922526b4e3a2b8f22fc7fe7b0a0e21a9f2e0b
Luan Nico <luanpotter27@gmail.com>
2020-02-16 11:33:50 -0500

    Fix release mode

--- game/lib/game/rumble.dart
+++ game/lib/game/rumble.dart
@@ -1,10 +1,10 @@
-import 'package:vibrate/vibrate.dart';
+import 'package:flutter/services.dart' show HapticFeedback;
 
 import 'preferences.dart';
 
 class Rumble {
   static rumble() {
     if (!Preferences.instance.rumbleOn) return;
-    Vibrate.vibrate();
+    HapticFeedback.vibrate();
   }
 }
