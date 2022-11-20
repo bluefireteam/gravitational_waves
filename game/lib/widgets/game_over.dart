bfa8d108ceb0922099458a8fb31bc39e2a1dc64a
Luke Pighetti <lukepighetti@gmail.com>
2022-11-19 20:59:37 -0500

    play again with space bar

--- game/lib/widgets/game_over.dart
+++ game/lib/widgets/game_over.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'button.dart';
 import 'gr_container.dart';
@@ -36,7 +37,7 @@ class _Line extends StatelessWidget {
   }
 }
 
-class GameOverContainer extends StatelessWidget {
+class GameOverContainer extends StatefulWidget {
   static const WIDTH = 350.0;
   static const HEIGHT = 320.0;
 
@@ -57,30 +58,54 @@ class GameOverContainer extends StatelessWidget {
   });
 
   @override
+  State<GameOverContainer> createState() => _GameOverContainerState();
+}
+
+class _GameOverContainerState extends State<GameOverContainer> {
+  late final keyboardListenerFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    keyboardListenerFocusNode.requestFocus();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GRContainer(
-      width: WIDTH,
-      height: HEIGHT,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Label(
-            label: 'Game Over',
-            fontSize: 36,
-            fontColor: PaletteColors.blues.dark,
-          ),
-          const SizedBox(height: 20),
-          _Line('Gems', '$gems'),
-          _Line('Distance', '$distance'),
-          const SizedBox(height: 20),
-          if (showExtraLifeButton)
-            PrimaryButton(
-              label: 'Extra life (ad)',
-              onPress: extraLife,
+    return KeyboardListener(
+      focusNode: keyboardListenerFocusNode,
+      autofocus: true,
+      onKeyEvent: (keyEvent) {
+        if (keyEvent is KeyDownEvent &&
+            keyEvent.logicalKey == LogicalKeyboardKey.space) {
+          widget.playAgain();
+        }
+      },
+      child: GRContainer(
+        width: GameOverContainer.WIDTH,
+        height: GameOverContainer.HEIGHT,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Label(
+              label: 'Game Over',
+              fontSize: 36,
+              fontColor: PaletteColors.blues.dark,
             ),
-          PrimaryButton(label: 'Play again', onPress: playAgain),
-          SecondaryButton(label: 'Back to menu', onPress: goToMainMenu),
-        ],
+            const SizedBox(height: 20),
+            _Line('Gems', '${widget.gems}'),
+            _Line('Distance', '${widget.distance}'),
+            const SizedBox(height: 20),
+            if (widget.showExtraLifeButton)
+              PrimaryButton(
+                label: 'Extra life (ad)',
+                onPress: widget.extraLife,
+              ),
+            PrimaryButton(label: 'Play again', onPress: widget.playAgain),
+            SecondaryButton(
+                label: 'Back to menu', onPress: widget.goToMainMenu),
+          ],
+        ),
       ),
     );
   }
